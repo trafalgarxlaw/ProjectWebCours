864d6e25153e77f10a1b6f0698eb880b91bd5c54
Trafalgar <masyyaf@gmail.com>
2019-10-19 20:29:59 -0400

    Language Update

---
+++ MyJavaScript.js
@@ -0,0 +1,8 @@
+function LangFunc(){
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+     if(selectedValue=="Eng"){
+        window.location.href = "indexEng.html";
+     }else{
+
+     }
+}
